6615a08b773a9cb698798714ded4109bd65b036e
Maxime <maximealberty@gmail.com>
2022-02-22 16:19:07 +0100

    Ajout couleur et commande (suite) Ajout de couleur dans les textes sorties sur cout. Ajout de la fusion pour la commande LIST, DELETE

--- mediatheque/src/main.cpp
+++ mediatheque/src/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <fstream>
 
+#include "../include/main.h"
 #include "../include/commande.h"
 #include "../include/mediatheque.h"
 #include "../include/livre.h"
@@ -32,11 +33,17 @@ int main (void) {
     string in_board, in_command, in_argument;
     int taille_board;
 
+    cout << COLOR_GREEN;
     cout << "Bienvenue dans le gestionnaire de médiathèque !" << endl << "Tapez HELP pour avoir la liste des commandes" << endl << endl;
-    
+    cout << COLOR_RESET;
     while(!exit_prog) {
         // Récupération de la commande et paramétrage
+        cout << endl
+             << COLOR_WHITE
+             << "Saisissez une commande >> "
+             << COLOR_BLUE;
         getline(cin, in_board);
+        cout << COLOR_RESET;
 
         taille_board = in_board.size();
         place_space = in_board.find(" ");
@@ -52,7 +59,7 @@ int main (void) {
 
         in_command = in_board.substr(0, place_space);
         in_command = str_to_upper (in_command);
-        cout << endl;
+        //cout << endl;
         // Test successif pour connaitre la commande. Un switch case n'est pas possible avec des string :'(
         // BYE : sortir
         if  (in_command == "BYE") {
@@ -62,7 +69,9 @@ int main (void) {
         // ADD : ajouter un media
         else if (in_command == "ADD") {
             if (in_argument == "") {
+                cout << COLOR_RED;
                 cout << "Pas d'argument!" << endl << endl;
+                cout << COLOR_RESET;
             }
             else {
                 cout <<"Ajout de : " << in_argument << endl << endl;
@@ -71,7 +80,9 @@ int main (void) {
         // LOAD : Chargement dans un fichier
         else if (in_command == "LOAD") {
             if (in_argument == "") {
+                cout << COLOR_RED;
                 cout << "Pas d'argument!" << endl << endl;
+                cout << COLOR_RESET;
             }
             else {
                 cout <<"chargement de : " << in_argument << endl << endl;
@@ -80,7 +91,9 @@ int main (void) {
         // SAVE : Sauvegarde de la médiathèque dans un fichier
         else if (in_command == "SAVE") {
             if (in_argument == "") {
+                cout << COLOR_RED;
                 cout << "Pas d'argument!" << endl << endl;
+                cout << COLOR_RESET;
             }
             else {
                 cout << "Sauvegarde de la médiathèque dans le fichier: " << in_argument << endl << endl;
@@ -89,7 +102,9 @@ int main (void) {
         // SEARCH : Recherche dans la médiathèque
         else if (in_command == "SEARCH") {
             if (in_argument == "") {
+                cout << COLOR_RED;
                 cout << "Pas d'argument!" << endl << endl;
+                cout << COLOR_RESET;
             }
             else {
                 cout << "Recherche de : " << in_argument << " dans la médiathèque" << endl << endl;
@@ -97,45 +112,72 @@ int main (void) {
         }
         // CLEAR : Clear de la recherche
         else if (in_command == "CLEAR") {
+            cout << COLOR_BOLDGREEN;
             cout << "Historique clear!" << endl << endl;
+            cout << COLOR_RESET;
         }
         // LIST : Liste la totalité des médias présent dans la médiathèque
         else if (in_command == "LIST") {
+            cout << COLOR_BOLDGREEN;
             cout << "Les médias présent dans la médiathèque sont : " << endl << endl;
+            cout << COLOR_RESET << COLOR_GREEN;
+            alpha.listMedia();
+            cout << COLOR_RESET;
         }
         // SHOW : Affichage des informations détaillées
         else if (in_command == "SHOW") {
             if (in_argument == "") {
+                cout << COLOR_RED;
                 cout << "Pas d'argument!" << endl << endl;
+                cout << COLOR_RESET;
             }
             else {
-                cout << "Afffichage des informations de l'ID " << in_argument << endl << endl;
+                cout << COLOR_GREEN;
+                cout << "Affichage des informations de l'ID " 
+                     << COLOR_RED
+                     << in_argument << endl << endl
+                     << COLOR_RESET;
                 alpha.showID(stoi(in_argument));
             }
         }
         // DELETE : Suppression d'une ressource
         else if (in_command == "DELETE") {
             if (in_argument == "") {
+                cout << COLOR_RED;
                 cout << "Pas d'argument!" << endl << endl;
+                cout << COLOR_RESET;
             }
             else {
-                cout << "Suppression de l'ID : " << in_argument << endl << endl;
+                cout << COLOR_GREEN;
+                cout << "Suppression de l'ID : " 
+                     << COLOR_RED
+                     << in_argument << endl << endl
+                     << COLOR_RESET;
+                alpha.deleteID(stoi(in_argument));
             }
         }
         // RESET : Remise à zero de la médiathèque
         else if (in_command == "RESET") {
+            cout << COLOR_RED;
             cout << "Etes-vous sur de vouloir tout supprimer (Y/N) :" << endl << endl;
+            cout << COLOR_RESET;
             getline(cin, in_board);
             in_board = str_to_upper (in_board);
             if (in_board == "Y") {
+                cout << COLOR_BOLDGREEN;
                 cout << "Reset de la médiathèque" << endl << endl;
+                cout << COLOR_RESET;
             }
             else {
+                cout << COLOR_BOLDGREEN;
                 cout << "Annulation du reset" << endl << endl;
+                cout << COLOR_RESET;
             }
         }
         else if (in_command == "HELP") {
+            cout << COLOR_BOLDGREEN;
             cout << "Liste des commandes : " << endl
+                 << COLOR_RESET << COLOR_GREEN 
                  << "BYE           : Quitte le progremmae" << endl
                  << "ADD t_media   : Ajout d'une nouvelle ressource de type 't_media'" << endl
                  << "LOAD filename : Charge le contenu du fichier filename  dans la médiathèque" << endl
@@ -145,12 +187,16 @@ int main (void) {
                  << "LIST          : Affichage de la base de donnée" << endl
                  << "SHOW id       : Affichage des informations du média à l'emplacement 'ID'" << endl
                  << "DELETE id     : Supprime le média ID de la base de donnée" << endl 
-                 << "RESET         : Remise à zéro de l'ensemble de la base de donnée" << endl << endl;
+                 << "RESET         : Remise à zéro de l'ensemble de la base de donnée" << endl << endl
+                 << COLOR_RESET;
         }
         // Commande non detecté
         else {
+            cout << COLOR_RED;
             cout << "La commande n'existe pas..." << endl;
+            cout << COLOR_GREEN;
             cout << "Veuillez réessayer" << endl << endl;
+            cout << COLOR_RESET;
         }
 
     }
